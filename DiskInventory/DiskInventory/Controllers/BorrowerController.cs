ed1173a2f273176455404e38d5cb5b22b0c6fe33
Kyle Killian <61425924+diglettusedswim@users.noreply.github.com>
2020-11-13 11:32:17 -0700

    added
    added

---
+++ DiskInventory/DiskInventory/Controllers/BorrowerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DiskInventory.Models;
+
+namespace DiskInventory.Controllers
+{
+    public class BorrowerController : Controller
+    {
+        private disk_inventorykkContext context { get; set; }
+        public BorrowerController(disk_inventorykkContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult List()
+        {
+            List<Borrower> borrowers = context.Borrower.OrderBy(a => a.Lname).ToList();
+            return View(borrowers);
+        }
+    }
+}
---
+++ DiskInventory/DiskInventory/Controllers/DiskController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DiskInventory.Models;
+
+namespace DiskInventory.Controllers
+{
+    public class DiskController : Controller
+    {
+        private disk_inventorykkContext context { get; set; }
+        public DiskController(disk_inventorykkContext ctx)
+        {
+            context = ctx;
+        }
+        public IActionResult List()
+        {
+            List<Disk> disks = context.Disk.OrderBy(a => a.DiskName).ToList();
+            return View(disks);
+        }
+    }
+}
